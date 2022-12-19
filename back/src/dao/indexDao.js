cf1721a31b64d3b64c805d586aeeb82e81ddd2fa
songyee <rizwell0222@gmail.com>
2022-12-19 18:10:09 +0900

    회원 검증 재시도

--- back/src/dao/indexDao.js
+++ back/src/dao/indexDao.js
@@ -13,6 +13,30 @@ exports.isValidUsers = async function(connection, userID, password) {
 };
 
 
+// DB 회원 검증(중복 아이디 확인)
+exports.checkID = async function (connection, userID) {
+  const Query = `SELECT userID FROM Users WHERE userID = ? AND status = 'A';`;
+  const Params = [userID];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+  
+  };
+  
+  
+// DB 회원 검증(중복 닉네임 확인)
+exports.checkNick = async function (connection, nickname) {
+  const Query = `SELECT nickname FROM Users WHERE nickname = ? AND status = 'A';`;
+  const Params = [nickname];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+    
+};
+
+
 // 회원 가입
 exports.insertUsers = async function(connection, userID, password, nickname) {
     const Query = `INSERT INTO Users(userID, password, nickname) VALUES(?, ?, ?);`;
